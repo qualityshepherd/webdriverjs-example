e48cfe5140b82d8d550e96dfbfed39e3aa85d3ad
Brine <brian@qualityshepherd.com>
2020-09-07 16:12:08 -0600

    add tests and refactor apiModule

--- tests/apiTest.js
+++ tests/apiTest.js
@@ -1,16 +1,44 @@
-import * as api from '../lib/apiModule';
+import api from '../lib/apiModule'
 
 describe('API', () => {
   test('should return a random fact', async () => {
-    const response = await api.getRandomFact();
+    const response = await api.get('/facts/random')
+    const json = await response.json()
 
-    expect(await response.status.sentCount).toBe(1);
-  });
+    expect(response.status).toBe(200)
+    expect(json.status.sentCount).toBe(1)
+  })
 
   test('should get cat by id', async () => {
-    const id = '5de780600013130015a3ccaf';
-    const response = await api.getCatById(id);
+    const id = '5de780600013130015a3ccaf'
+    const response = await api.get(`/facts/${id}`)
+    const json = await response.json()
 
-    expect(await response._id).toBe(id);
-  });
+    expect(response.status).toBe(200)
+    expect(json._id).toBe(id)
+  })
+
+  test('should get facts by type', async () => {
+    const response = await api.get(`/facts/random?animal_type=cat`)
+    const json = await response.json()
+
+    expect(response.status).toBe(200)
+    expect(json.type).toBe('cat')
+  })
+
+  test('should get facts by amount', async () => {
+    const response = await api.get(`/facts/random?amount=2`)
+    const json = await response.json()
+
+    expect(response.status).toBe(200)
+    expect(json.length).toBe(2)
+  })
+
+  test('should error on invalid id', async () => {
+    const response = await api.get('/facts/invalid_id')
+    const json = await response.json()
+
+    expect(response.status).toBe(400)
+    expect(json.message).toBeDefined()
+  })
 })
